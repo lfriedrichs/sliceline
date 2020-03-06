887641db3a4b91467e478c44313f04d75b501ed7
github username <github email address>
2020-03-06 12:08:22 -0800

    autofill new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,24 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
+    if session[:user]
+      @user = User.new(session[:user])
+      session.delete :user
+    else
+      @user = User.new
+    end
   end
 
   def create
+    session[:user] = {}
+    auto_fill(session[:user])
     @user = User.create(user_params)
     if @user.errors
       flash[:message] = @user.errors.messages
       redirect_to new_user_path
     else
       session[:user_id] = @user.id
+      session.delete :user
       redirect_to @user
     end
   end
@@ -46,6 +54,19 @@ class UsersController < ApplicationController
       :user_name, :password, :password_confirmation, :email, 
       :phone_number, :address_line_1, :address_line_2, :city, 
       :state, :zipcode)
+  end
+
+  def auto_fill(user)
+    user[:first_name] = params[:user][:first_name]
+    user[:last_name] = params[:user][:last_name]
+    user[:user_name] = params[:user][:user_name]
+    user[:email] = params[:user][:email]
+    user[:phone_number] = params[:user][:phone_number]
+    user[:address_line_1] = params[:user][:address_line_1]
+    user[:address_line_2] = params[:user][:address_line_2]
+    user[:city] = params[:user][:city]
+    user[:state] = params[:user][:state]
+    user[:zipcode] = params[:user][:zipcode]
   end
 
 end
